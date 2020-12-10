85c05278b06224604720dfc702439ca317923c0b
mariamoljames <mariamol.james@gmail.com>
2020-12-09 22:15:35 -0500

    Done.

--- lib/oo_largest_prime_factor.rb
+++ lib/oo_largest_prime_factor.rb
@@ -1,1 +1,15 @@
-# Enter your object-oriented solution here!
+class LargestPrimeFactor
+    attr_accessor :number
+    def initialize(input)
+        @number = largest_prime_factor(input)
+    end
+    def largest_prime_factor(input)
+        @number=input
+        for i in 2..Math.sqrt(@number).round()
+            while(@number%i==0 && @number>i)
+                @number/=i
+            end
+        end
+        @number
+    end
+end
